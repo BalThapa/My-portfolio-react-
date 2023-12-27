dfc2abb1ff2ed0aabbb1df1e23b563ff02119608
BalThapa <info.balthapa@gmail.com>
2023-12-27 21:04:25 +0200

    Waving hand deleted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Profile from '../assets/profile.jpeg';
 import Resume from '../assets/bal-resume.pdf';
-
-import {FaArrowRight} from 'react-icons/fa'
+import {MdArrowForward} from 'react-icons/md'
 import Socials from './Socials';
 
 const Hero = () => {
-
+ 
     return (
         <section className='min-h-screen flex flex-col justify-start items-center p-5 text-center'>
     
@@ -14,19 +13,9 @@ const Hero = () => {
             <h2 className='text-3xl uppercase font-bold text-rose-600'>Bal Krishna Thapa Chhetri</h2>
             <h3 className='py-3 text-xl'>Junior Software Developer</h3>
             <p className='max-w-xl font-light text-gray-500'>
-                Hello <span className='animate-pulse text-3xl'>👋</span>, welcome to my portfolio. I am a Junior Software Developer searching for an internship opportunity. I love to work in FrontEndside and learning backend also. 
+                Hello, welcome to my portfolio. I am a Junior Software Developer searching for an internship opportunity. I love to work in FrontEndside and learning backend also. 
             </p>
             </div> 
-            {/* <div className='flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3'>
-                {SOCIAL.map(({id, link, icon}) => (
-                   <a href={link} 
-                        key={id} 
-                        target='_blank'
-                        rel='noopener noreferrer'
-                        className='cursor-pointer duration-300 hover:text-rose-600'
-                        >{icon}</a> 
-                ))}
-            </div>  */}
             <Socials/>
             <div>
                 <img 
@@ -34,11 +23,12 @@ const Hero = () => {
                     alt="pic" 
                     className="w-60 h-60 md:w-72 object-cover rounded-xl"
                 />
+                
                 <a 
                 href={Resume} 
                 target='_blank'
-                rel='noopener noreferrer' className='flex items-center justify-center mt-10 bg-gradient-to-r from-red-600 to-teal-500 text-white py-2 rounded-lg'>Resume
-                <FaArrowRight className='flex ml-5 text-white text-3xl transition-transform duration-500 hover:-translate-x-4'/>
+                rel='noopener noreferrer' className='flex items-center justify-center mt-10 bg-gradient-to-r from-red-600 to-teal-500 text-white py-2 rounded-lg'> Resume
+                <MdArrowForward className='flex ml-5 text-white text-2xl transition-transform duration-500 hover:-translate-x-4'/>
                 </a>
                 
             </div> 
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,7 +5,7 @@ import { MdClose } from 'react-icons/md';
 
 const Menu = () => {
     return (
-        <div className='flex'>
+        <div className='flex '>
                 <ul className='lg:flex md:flex hidden '>
                     <li className='mr-6 '>
                         <a href='/'>Home</a>
@@ -25,7 +25,7 @@ const Menu = () => {
                 </ul>
                 <button className='space-y-1 group md:hidden'>
                     <GiHamburgerMenu className='flex md:hidden lg:hidden'/>
-                    <ul className='bg-[#252525] w-screen pb-10 right-0 absolute -top-full group-focus-top-0 duration-150 flex flex-col justify-end text-red-200 '>
+                    <ul className='bg-[#252525] w-screen pb-10 right-0 absolute -top-full group-focus:top-0 duration-150 flex flex-col justify-end text-red-200 '>
                         <button className='px-10 py-8 relative ml-auto text-2xl'>
                             <MdClose/>
                         </button>
