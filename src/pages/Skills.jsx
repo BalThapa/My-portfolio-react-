f997c625874441003262f93ce6bde743b1f0bdd7
BalThapa <info.balthapa@gmail.com>
2024-01-02 22:24:42 +0200

    Skills section done

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Card1 from '../components/ui/Card1';
-import Js from '../assets/Js.png';
+import Js from '../assets/js.jpeg';
 import Html from '../assets/html5.png';
+import Reactjs from '../assets/react.png';
+import Reduxjs from '../assets/redux.jpeg';
+import Nextjs from '../assets/nextjs.png';
+import CSS from '../assets/css.png';
+import BS from '../assets/Bootstrap.png';
+import TW from '../assets/tw.png';
+import Ts from '../assets/TS.png';
+import PHP from '../assets/php.jpg';
+import MySql from '../assets/mysql.png';
+import Drupal from '../assets/drupal.png';
+import FireBase from '../assets/firebase.png';
+import Node from '../assets/njs.jpeg';
+import Git from '../assets/GitHub.png';
+import Docker from '../assets/Docker.jpeg';
+import AWS from '../assets/aws.png';
+import Lando from '../assets/lando.png';
+import Sf from '../assets/sf.png';
+import Figma from '../assets/figma.jpeg';
+import Trello from '../assets/trello.jpeg';
  
 const Skills = () => {
     return (
@@ -10,88 +29,138 @@ const Skills = () => {
             <p>While moving along with my life and studying BBA from Centria UAS and Full Stack Development program from Business College Helsinki, I get the opportunity to sharpen my diverse skills set in both technical and soft skills. With 
                 a lot of study, practice, failure and success I was able to get myself acquinted with various technologies and tools. Below are the skills and level.  </p>
             <div>
-                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Skills</h2>
+                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Frontend Technologies</h2>
+                <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>            
+                    <Card1 
+                    image={Html}
+                    title='HTML'
+                    width='80%'
+                    />
+                    <Card1 
+                    image={CSS}
+                    title='CSS'
+                    width='80%'
+                    />
+                    <Card1 
+                    image={Reactjs}
+                    title='React'
+                    width='70%'
+                    />
+                    <Card1 
+                    image={Reduxjs}
+                    title='Redux'
+                    width='30%'
+                    />
+                   
+                </div>
+                <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'> 
+                    <Card1 
+                    image={Nextjs}
+                    title='Nextjs'
+                    width='20%'
+                    />           
+                    <Card1 
+                    image={BS}
+                    title='Bootstrap'
+                    width='80%'
+                    />
+                    <Card1 
+                    image={TW}
+                    title='Tailwind'
+                    width='80%'
+                    />
+                                   
+                </div>
+            </div>
+            <div>
+                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Programming Languages</h2>
                 <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>            
                     <Card1 
                     image={Js}
-                    subtitle='JavaScript'
-                    width='50%'
-                    />
-                    <Card1 
-                    image={Html}
-                    subtitle='JavaScript'
-                    width='50%'
-                    />
-                    <Card1 
                     title='JavaScript'
-                    width='50%'
+                    width='60%'
                     />
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Ts}
+                    title='TypeScript'
+                    width='30%'
+                    />
+                    <Card1
+                    image={PHP} 
+                    title='PHP'
+                    width='40%'
                     />
                 </div>
             </div>
             <div>
-                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Skills</h2>
+                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Backend Techologies & CMS</h2>
                 <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>            
                     <Card1 
-                    title='JavaScript'
+                    image={MySql} 
+                    title='MySQL'
                     width='50%'
                     />
-                    <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    <Card1
+                    image={FireBase}  
+                    title='Firebase'
+                    width='30%'
                     />
-                    <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    <Card1
+                    image={Node}  
+                    title='node'
+                    width='40%'
                     />
-                    <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    <Card1
+                    image={Sf}  
+                    title='Symfony'
+                    width='40%'
+                    />
+                </div>
+                <div className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>
+                    <Card1
+                    image={Drupal}  
+                    title='Drupal'
+                    width='70%'
                     />
                 </div>
             </div>
             <div>
-                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Skills</h2>
+                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>DevOp Technologies</h2>
                 <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>            
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Git}  
+                    title='Github'
+                    width='40%'
                     />
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Docker}  
+                    title='Docker'
+                    width='40%'
                     />
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={AWS}  
+                    title='AWS'
+                    width='30%'
                     />
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Lando}  
+                    title='Lando'
+                    width='40%'
                     />
                 </div>
             </div>
             <div>
-                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Skills</h2>
+                <h2 className='flex justify-center py-10 text-xl lg:text-2xl lg:justify-start font-bold text-red-900'>Design & Project Management Tools</h2>
                 <div  className='flex flex-col gap-10 items-center p-10 lg:flex-row lg:justify-center'>            
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Figma}  
+                    title='Figma'
+                    width='70%'
                     />
                     <Card1 
-                    title='JavaScript'
-                    width='50%'
-                    />
-                    <Card1 
-                    title='JavaScript'
-                    width='50%'
-                    />
-                    <Card1 
-                    title='JavaScript'
-                    width='50%'
+                    image={Trello}  
+                    title='Trello'
+                    width='70%'
                     />
                 </div>
             </div>
