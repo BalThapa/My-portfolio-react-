9fc965d0901d9becd90590eb35d1554c7a651187
BalThapa <info.balthapa@gmail.com>
2024-01-07 13:59:48 +0200

    some trimming is done

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,57 +1,3 @@
-// import React from 'react';
-// import { GiHamburgerMenu } from 'react-icons/gi';
-// import { MdClose } from 'react-icons/md';
-
-
-// const Menu = () => {
-//     return (
-//         <div className='flex '>
-//                 <ul className='lg:flex md:flex hidden'>
-//                     <li className='mr-6 '>
-//                         <a href='/' className='hover:underline hover:text-blue-900'>Home</a>
-//                     </li>
-//                     <li className='mr-6'>
-//                         <a href='/about' className='hover:underline hover:text-blue-900'>About</a>
-//                     </li>
-//                     <li className='mr-6'>
-//                         <a href='/skills' className='hover:underline hover:text-blue-900'>Skills</a>
-//                     </li>
-//                     <li className='mr-6' >
-//                         <a href='/projects' className='hover:underline hover:text-blue-900'>Projects</a>
-//                     </li>
-//                     <li className='mr-6'>
-//                         <a href='/contact' className='hover:underline hover:text-blue-900'>Contact</a>
-//                     </li>
-//                 </ul>
-//                 <button className='space-y-1 group md:hidden'>
-//                     <GiHamburgerMenu className='flex md:hidden lg:hidden'/>
-//                     <ul className='bg-gray-900 w-screen pb-10 z-10 right-0 absolute -top-full group-focus:top-0 duration-150 flex flex-col justify-end text-red-200 '>
-//                         <button className='px-10 py-8 relative ml-auto text-2xl'>
-//                             <MdClose/>
-//                         </button>
-//                         <li className='flex justify-center w-full py-4 hover:bg-black'>
-//                             <a href='/'>Home</a>
-//                         </li>
-//                         <li className='flex justify-center w-full py-4 hover:bg-black'>
-//                             <a href='/about'>About</a>
-//                         </li>
-//                         <li className='flex justify-center w-full py-4 hover:bg-black'>
-//                             <a href='/skills'>Skills</a>
-//                         </li>
-//                         <li className='flex justify-center w-full py-4 hover:bg-black'>
-//                             <a href='/projects'>Projects</a>
-//                         </li>
-//                         <li className='flex justify-center w-full py-4 hover:bg-black'>
-//                             <a href='/contact'>Contact</a>
-//                         </li>
-//                 </ul>
-//                 </button>
-//         </div>
-//     );
-// };
-
-// export default Menu;
-
 import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdClose } from 'react-icons/md';
@@ -86,7 +32,7 @@ const Menu = () => {
                 {isNavOpen ? <MdClose className='flex md:hidden lg:hidden' /> : <GiHamburgerMenu className='flex md:hidden lg:hidden' />}
             </button>
             {isNavOpen && (
-                <ul className='fixed top-0 left-0 w-full  bg-gray-900 text-red-200 z-50 flex flex-col justify-end'>
+                <ul className='fixed top-0 left-0 w-full bg-gray-900 text-red-200 z-10 flex flex-col justify-end'>
                     <button className='px-10 py-8 relative ml-auto text-2xl' onClick={toggleNav}>
                         <MdClose/>
                     </button>
