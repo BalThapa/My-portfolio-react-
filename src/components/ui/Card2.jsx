3fcb5ac347e8ca35d7c7dff4989d2555bb41f408
BalThapa <info.balthapa@gmail.com>
2024-01-17 09:50:03 +0200

    some paddings adjustments

--- src/components/ui/Card2.jsx
+++ src/components/ui/Card2.jsx
@@ -4,7 +4,7 @@ import {FaGithub, FaLink} from 'react-icons/fa';
 
 const Card2 = ({ video, title, subtitle, link1, link2, keywords}) => {
   return (
-    <div className="flex bg-slate-100 flex-col dark:bg-gray-800 md:flex-col lg:flex-row h-auto p-5 lg:h-auto items-center justify-start shadow-md shadow-gray-500 dark:shadow-gray-100 rounded-xl duration-300 ease-in-out hover:scale-110 ">
+    <div className="flex bg-slate-100 flex-col dark:bg-gray-800 md:flex-col lg:flex-row h-auto p-3 lg:h-auto items-center justify-start shadow-md shadow-gray-500 dark:shadow-gray-100 rounded-xl duration-300 ease-in-out hover:scale-110 ">
       <video src={video} type='video/mp4' controls autoPlay muted className="w-96 rounded-xl" />
       <div className="flex flex-col items-center text-center w-full pt-3">
         <h2 className="text-2xl">{title}</h2>
